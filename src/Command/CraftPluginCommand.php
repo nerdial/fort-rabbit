e8ba55a2eb0a66a14e7c59cd8d709b154dd4fc0e
Vahid Mahdiun <v.mahdiun@yahoo.com>
2021-12-09 03:02:47 +0330

    Refactored craft plugin command and added better functionality.

--- src/Command/CraftPluginCommand.php
+++ src/Command/CraftPluginCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Traits\PackagistApi;
 use App\Traits\Sortable;
 use App\Entity\CraftPluginPackage;
 use Symfony\Component\Console\Command\Command;
@@ -10,28 +11,29 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use GuzzleHttp\Client;
 use Symfony\Component\Console\Helper\Table;
-use GuzzleHttp\Promise;
+
 
 class CraftPluginCommand extends Command
 {
 
     use Sortable;
+    use PackagistApi;
 
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'craft:analyze';
 
     protected Client $client;
-    protected string $packageName = "craft-plugin";
+    protected string $type = "craft-plugin";
     protected string $orderBy = 'downloads';
     protected string $order = 'desc';
     protected int $perPage = 50;
     protected array $orderByOptions = [
         'downloads', 'favers', 'dependents', 'updated'
     ];
+    protected array $abandoneds = [];
 
     protected function configure(): void
     {
-
         $this->setHelp('This command allows you to analyze craft cms plugins.');
 
         $this->addOption('limit', 'l', InputArgument::OPTIONAL,
@@ -54,43 +56,68 @@ class CraftPluginCommand extends Command
         ]);
     }
 
+    /**
+     * @throws \Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $result = $this->callApiToGetAllPackageNames($input);
-
-        $packageNames = array_column($result, 'name');
-
-        $packages = $this->callApiToGetEachPackage($packageNames);
-
 
         $orderBy = $input->getOption('orderBy');
-
         if (!in_array(strtolower($orderBy), $this->orderByOptions)) {
             throw new \Exception($orderBy . ' is not supported as order');
         }
 
+        $packages = $this->getAllPackagesFromPackagist();
 
-        $packages = $this->sortTheResult($input, $result, $orderBy);
+        // create abandoned filter
+        $this->createBlacklist($packages);
 
+        $limitedResult = $this->getLimitedResultFromPackagist($input);
+
+        // filter the result and remove abandoned packages
+        $result = $this->removeBlacklistFromResult($limitedResult);
+
+
+        $packageNames = array_column($result, 'name');
+        $packages = $this->getEachPackageSeparately($packageNames);
+
+
+        $packages = $this->sortResult($input, $packages, $orderBy);
         $this->createOutput($input, $output, $packages);
-
 
         return Command::SUCCESS;
     }
 
+    protected function createBlacklist($packages)
+    {
+        foreach ($packages as $packageName => $value) {
+            if ($value['abandoned'] != false) $this->abandoneds [] = $packageName;
+        }
+    }
+
+    protected function removeBlacklistFromResult($result): array
+    {
+        return array_filter($result, function ($item) {
+            return !in_array($item['name'], $this->abandoneds);
+        });
+    }
 
     protected function createOutput(InputInterface $input, OutputInterface $output, array $packages)
     {
-        $headers = ['Name', 'Description', 'Url', 'Repository', 'Downloads', 'Favers'];
+
+
+        $headers = ['Name', 'Description', 'Updated', 'Handle', 'Repository', 'Downloads', 'Dependents', 'Favers'];
         $data = [];
         foreach ($packages as $item) {
             $data [] = [
                 $item['name'],
                 $item['description'],
-                $item['url'],
+                $item['updated'],
+                $item['handle'],
                 $item['repository'],
                 $item['downloads'],
-                $item['favers']
+                $item['dependents'],
+                $item['favers'],
             ];
         }
 
@@ -106,48 +133,5 @@ class CraftPluginCommand extends Command
         }
     }
 
-    protected function callApiToGetAllPackageNames(InputInterface $input)
-    {
-        $response = $this->client->request('GET', 'search.json', [
-            'query' => [
-                'type' => $this->packageName,
-                'abandoned' => false,
-                'per_page' => $input->getOption('limit')
-            ],
-        ]);
-        return json_decode($response->getBody()->getContents(), true)['results'];
-    }
-
-    protected function callApiToGetEachPackage(array $names)
-    {
-        // generate request for all packages
-        $requests = [];
-        foreach ($names as $name) {
-            $packageUri = 'packages/' . $name . '.json';
-            $requests[$name] = $this->client->getAsync($packageUri);
-        }
-
-        $responses = Promise\Utils::settle($requests)->wait();
-        $packages = [];
-        foreach ($responses as $response) {
-
-            $result = $response['value'];
-            $data = json_decode($result->getBody()->getContents(), true)['package'];
-
-            $package = new CraftPluginPackage();
-            $package->name = $data['name'];
-            $package->description = $data['description'];
-            $package->updated = new \DateTime($data['time']);
-            $package->repository = $data['repository'];
-            $package->version = $data['repository'];
-            $package->downloads = $data['downloads']['total'];
-            $package->dependents = $data['dependents'];
-            $package->favers = $data['favers'];
-
-            array_push($packages, $package);
-
-        }
-        return $packages;
-    }
 
 }
